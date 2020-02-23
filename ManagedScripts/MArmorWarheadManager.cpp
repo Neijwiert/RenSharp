4d616a960f12a508c85e1a8d4e89defbe51dffdb
Neijwiert <guidoknapen@gmail.com>
2020-02-23 15:22:52 +0100

    Added missing methods in ArmorWarheadManager

--- ManagedScripts/MArmorWarheadManager.cpp
+++ ManagedScripts/MArmorWarheadManager.cpp
@@ -17,7 +17,186 @@ limitations under the License.
 #include "stdafx.h"
 #include "MArmorWarheadManager.h"
 
+using namespace System;
+using namespace System::Runtime::InteropServices;
+
 namespace RenSharp
 {
+	bool ArmorWarheadManager::IsArmorSoft(unsigned int armor)
+	{
+		return ::ArmorWarheadManager::Is_Armor_Soft(armor);
+	}
 
+	unsigned int ArmorWarheadManager::GetArmorType(String^ name)
+	{
+		if (name == nullptr)
+		{
+			throw gcnew ArgumentNullException("name");
+		}
+
+		IntPtr namePtr = Marshal::StringToHGlobalAnsi(name);
+		try
+		{
+			return ::ArmorWarheadManager::Get_Armor_Type(reinterpret_cast<const char*>(namePtr.ToPointer()));
+		}
+		finally
+		{
+			Marshal::FreeHGlobal(namePtr);
+		}
+	}
+
+	String^ ArmorWarheadManager::GetArmorName(unsigned int type)
+	{
+		if (type > NumArmorTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+
+		auto result = ::ArmorWarheadManager::Get_Armor_Name(type);
+		if (result == nullptr)
+		{
+			return nullptr;
+		}
+		else
+		{
+			return gcnew String(result);
+		}
+	}
+
+	unsigned int ArmorWarheadManager::GetWarheadType(String^ name)
+	{
+		if (name == nullptr)
+		{
+			throw gcnew ArgumentNullException("name");
+		}
+
+		IntPtr namePtr = Marshal::StringToHGlobalAnsi(name);
+		try
+		{
+			return ::ArmorWarheadManager::Get_Warhead_Type(reinterpret_cast<const char*>(namePtr.ToPointer()));
+		}
+		finally
+		{
+			Marshal::FreeHGlobal(namePtr);
+		}
+	}
+
+	String^ ArmorWarheadManager::GetWarheadName(unsigned int type)
+	{
+		if (type > NumWarheadTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+
+		auto result = ::ArmorWarheadManager::Get_Warhead_Name(type);
+		if (result == nullptr)
+		{
+			return nullptr;
+		}
+		else
+		{
+			return gcnew String(result);
+		}
+	}
+
+	float ArmorWarheadManager::GetDamageMultiplier(unsigned int armor, unsigned int warhead)
+	{
+		if (armor > NumArmorTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+		else if (warhead > NumWarheadTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+
+		return ::ArmorWarheadManager::Get_Damage_Multiplier(armor, warhead);
+	}
+
+	float ArmorWarheadManager::GetShieldAbsorbsion(unsigned int armor, unsigned int warhead)
+	{
+		if (armor > NumArmorTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+		else if (warhead > NumWarheadTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+
+		return ::ArmorWarheadManager::Get_Shield_Absorbsion(armor, warhead);
+	}
+
+	ArmorWarheadManager::SpecialDamageType ArmorWarheadManager::GetSpecialDamageType(unsigned int warhead)
+	{
+		if (warhead > NumWarheadTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+
+		return static_cast<SpecialDamageType>(::ArmorWarheadManager::Get_Special_Damage_Type(warhead));
+	}
+
+	float ArmorWarheadManager::GetSpecialDamageProbability(unsigned int warhead)
+	{
+		if (warhead > NumWarheadTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+
+		return ::ArmorWarheadManager::Get_Special_Damage_Probability(warhead);
+	}
+
+	unsigned int ArmorWarheadManager::GetSpecialDamageWarhead(SpecialDamageType type)
+	{
+		return ::ArmorWarheadManager::Get_Special_Damage_Warhead(static_cast<::ArmorWarheadManager::SpecialDamageType>(type));
+	}
+
+	float ArmorWarheadManager::GetSpecialDamageDuration(SpecialDamageType type)
+	{
+		return ::ArmorWarheadManager::Get_Special_Damage_Duration(static_cast<::ArmorWarheadManager::SpecialDamageType>(type));
+	}
+
+	float ArmorWarheadManager::GetSpecialDamageScale(SpecialDamageType type)
+	{
+		return ::ArmorWarheadManager::Get_Special_Damage_Scale(static_cast<::ArmorWarheadManager::SpecialDamageType>(type));
+	}
+
+	String^ ArmorWarheadManager::GetSpecialDamageExplosion(SpecialDamageType type)
+	{
+		auto result = ::ArmorWarheadManager::Get_Special_Damage_Explosion(static_cast<::ArmorWarheadManager::SpecialDamageType>(type));
+		if (result == nullptr)
+		{
+			return nullptr;
+		}
+		else
+		{
+			return gcnew String(result);
+		}
+	}
+
+	float ArmorWarheadManager::GetVisceroidProbability(unsigned int warhead)
+	{
+		if (warhead > NumWarheadTypes)
+		{
+			throw gcnew ArgumentOutOfRangeException("type");
+		}
+
+		return ::ArmorWarheadManager::Get_Visceroid_Probability(warhead);
+	}
+
+	bool ArmorWarheadManager::IsSkinImpervious(SpecialDamageType type, unsigned int skin)
+	{
+		return ::ArmorWarheadManager::Is_Skin_Impervious(static_cast<::ArmorWarheadManager::SpecialDamageType>(type), skin);
+	}
+
+	unsigned int ArmorWarheadManager::NumArmorTypes::get()
+	{
+		return static_cast<unsigned int>(::ArmorWarheadManager::Get_Num_Armor_Types());
+	}
+
+	unsigned int ArmorWarheadManager::NumWarheadTypes::get()
+	{
+		return static_cast<unsigned int>(::ArmorWarheadManager::Get_Num_Warhead_Types());
+	}
 }
--- ManagedScripts/MArmorWarheadManager.h
+++ ManagedScripts/MArmorWarheadManager.h
@@ -40,5 +40,31 @@ namespace RenSharp
 				SpecialDamageTypeCnCChem = ::ArmorWarheadManager::SPECIAL_DAMAGE_TYPE_CNC_CHEM,
 				SpecialDamageTypeSuperFire = ::ArmorWarheadManager::SPECIAL_DAMAGE_TYPE_SUPER_FIRE
 			};
+
+			static bool	IsArmorSoft(unsigned int armor);
+			static unsigned int GetArmorType(System::String^ name);
+			static System::String^ GetArmorName(unsigned int type);
+			static unsigned int	GetWarheadType(System::String^ name);
+			static System::String^ GetWarheadName(unsigned int type);
+			static float GetDamageMultiplier(unsigned int armor, unsigned int warhead);
+			static float GetShieldAbsorbsion(unsigned int armor, unsigned int warhead);
+			static SpecialDamageType GetSpecialDamageType(unsigned int warhead);
+			static float GetSpecialDamageProbability(unsigned int warhead);
+			static unsigned int GetSpecialDamageWarhead(SpecialDamageType type);
+			static float GetSpecialDamageDuration(SpecialDamageType type);
+			static float GetSpecialDamageScale(SpecialDamageType type);
+			static System::String^ GetSpecialDamageExplosion(SpecialDamageType type);
+			static float GetVisceroidProbability(unsigned int warhead);
+			static bool	IsSkinImpervious(SpecialDamageType type, unsigned int skin);
+
+			static property unsigned int NumArmorTypes
+			{
+				unsigned int get();
+			}
+
+			static property unsigned int NumWarheadTypes
+			{
+				unsigned int get();
+			}
 	};
 }
