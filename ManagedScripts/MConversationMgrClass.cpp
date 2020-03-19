ba61ef739a904446dcb9f75082110231d540c2e3
Neijwiert <guidoknapen@gmail.com>
2020-03-19 17:14:27 +0100

    Added ConversationMgrClass to find conversations

---
+++ ManagedScripts/MConversationMgrClass.cpp
@@ -0,0 +1,117 @@
+/*
+Copyright 2020 Neijwiert
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "stdafx.h"
+#include  "MConversationMgrClass.h"
+
+#pragma managed(push, off)
+#pragma warning(push)
+#pragma warning(disable : 4251 4244 26495 26454)
+
+class ConversationClass;
+
+class ConversationMgrClass : public SaveLoadSubSystemClass
+{
+	public:
+		static ::ConversationClass* Find_Conversation(const char* name);
+		static ::ConversationClass* Find_Conversation(int id);
+};
+
+RENEGADE_FUNCTION
+::ConversationClass* ::ConversationMgrClass::Find_Conversation(const char* name)
+AT2(0x006D6C30, 0x006D64D0);
+
+RENEGADE_FUNCTION
+::ConversationClass* ::ConversationMgrClass::Find_Conversation(int id)
+AT2(0x006D6C90, 0x006D6530);
+
+::ConversationClass* Find_Conversation(const char* name)
+{
+	return ::ConversationMgrClass::Find_Conversation(name);
+}
+
+::ConversationClass* Find_Conversation(int id)
+{
+	return ::ConversationMgrClass::Find_Conversation(id);
+}
+
+#pragma warning(pop) 
+#pragma managed(pop)
+
+#include "MConversationClass.h"
+
+namespace RenSharp
+{
+	ConversationMgrClass::ConversationMgrClass(IntPtr pointer)
+		: SaveLoadSubSystemClass(pointer)
+	{
+
+	}
+
+	IConversationClass^ ConversationMgrClass::FindConversation(String^ name)
+	{
+		if (name == nullptr)
+		{
+			throw gcnew ArgumentNullException("name");
+		}
+
+		IntPtr nameHandle = Marshal::StringToHGlobalAnsi(name);
+		try
+		{
+			auto result = Find_Conversation(reinterpret_cast<char*>(nameHandle.ToPointer()));
+			if (result == nullptr)
+			{
+				return nullptr;
+			}
+			else
+			{
+				return gcnew ConversationClass(IntPtr(result));
+			}
+		}
+		finally
+		{
+			Marshal::FreeHGlobal(nameHandle);
+		}
+	}
+
+	IConversationClass^ ConversationMgrClass::FindConversation(int id)
+	{
+		auto result = Find_Conversation(id);
+		if (result == nullptr)
+		{
+			return nullptr;
+		}
+		else
+		{
+			return gcnew ConversationClass(IntPtr(result));
+		}
+	}
+
+	IntPtr ConversationMgrClass::ConversationMgrClassPointer::get()
+	{
+		return IntPtr(InternalConversationMgrClassPointer);
+	}
+
+	::SaveLoadSubSystemClass* ConversationMgrClass::InternalSaveLoadSubSystemClassPointer::get()
+	{
+		return InternalConversationMgrClassPointer;
+	}
+
+	::ConversationMgrClass* ConversationMgrClass::InternalConversationMgrClassPointer::get()
+	{
+		return reinterpret_cast<::ConversationMgrClass*>(Pointer.ToPointer());
+	}
+}
---
+++ ManagedScripts/MConversationMgrClass.h
@@ -0,0 +1,65 @@
+/*
+Copyright 2020 Neijwiert
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include "MSaveLoadSubSystemClass.h"
+
+using namespace System;
+
+#pragma managed(push, off)
+
+class ConversationMgrClass;
+
+#pragma managed(pop)
+
+namespace RenSharp
+{
+	interface class IConversationClass;
+
+	public interface class IConversationMgrClass : public ISaveLoadSubSystemClass
+	{
+		property IntPtr ConversationMgrClassPointer
+		{
+			IntPtr get();
+		}
+	};
+
+	public ref class ConversationMgrClass : public SaveLoadSubSystemClass, public IConversationMgrClass
+	{
+		public:
+			ConversationMgrClass(IntPtr pointer);
+
+			static IConversationClass^ FindConversation(String^ name);
+			static IConversationClass^ FindConversation(int id);
+
+			property IntPtr ConversationMgrClassPointer
+			{
+				virtual IntPtr get() sealed;
+			}
+
+		protected:
+			property ::SaveLoadSubSystemClass* InternalSaveLoadSubSystemClassPointer
+			{
+				::SaveLoadSubSystemClass* get() override;
+			}
+
+			property ::ConversationMgrClass* InternalConversationMgrClassPointer
+			{
+				virtual ::ConversationMgrClass* get();
+			}
+	};
+}
